d37632b2957a4298941d42e1e47c8e70f302ba7f
rahmanrusmana <rahmanrusmana27@gmail.com>
2023-08-07 14:07:56 +0700

    belajar-mocule-vuex

---
+++ src/store/modules/clients.js
@@ -0,0 +1,19 @@
+const clients = {
+    namespaced: true,
+    state: {
+        listBantuan: [
+            {name: "Gempa Lombok"},
+            {name: "Beasiswa Pendidikan"},
+            {name: "Banjir Bandang"},
+            {name: "Tanggungan Kesehatan"},
+        ],
+    },
+    mutations: {
+        //None
+    },
+    actions: {
+        //None
+    }
+}
+
+export default clients;
---
+++ src/store/modules/donatur.js
@@ -0,0 +1,19 @@
+const donatur = {
+    namespaced: true,
+    state: {
+        listDonatur: [
+            {name: "Riski Amelia"},
+            {name: "Ima Sumadir"},
+            {name: "Apri Yunita"},
+            {name: "Tuti Winarti"},
+        ],
+    },
+    mutations: {
+        //None
+    },
+    actions: {
+        //None
+    }
+}
+
+export default donatur;
